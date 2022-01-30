3e433fa8c2b14c7ac6a4e3adf0eb9c10b53e9ce1
Matt <mtomko87@gmail.com>
2022-01-30 15:28:17 -0500

    fix import

--- src/components/ScoreInfo.js
+++ src/components/ScoreInfo.js
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import styled from "styled-components";
 import EyeIcon from "../icons/EyeIcon";
 import { useGame } from "../providers/GameProvider";
