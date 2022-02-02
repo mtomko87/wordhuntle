7a35c72f287a231aefd1b8dc195f8029d8b6e422
Matt <mtomko87@gmail.com>
2022-02-01 21:29:27 -0500

    remove aspect ratio and use padding-top instead, because my dumbass dad wont update his phone

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,9 +2,19 @@ import styled from "styled-components";
 import Square from "./Square";
 import { boardSize } from "../utils/utils";
 
-const Div = styled.div`
+const Container = styled.div`
     width: 100%;
-    aspect-ratio: 1;
+    height: 0;
+    padding-top: 100%;
+    position: relative;
+`
+
+const StyledBoard = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
     display: grid;
     grid-template-columns: repeat(${boardSize}, 1fr);
     grid-gap: 0.375rem;
@@ -12,15 +22,17 @@ const Div = styled.div`
 
 const Board = () => {
     return (
-        <Div>
-            {[...Array(boardSize * boardSize)].map((_, index) => (
-                <Square
-                    key={index}
-                    x={index % boardSize}
-                    y={boardSize - Math.floor(index / boardSize) - 1}
-                />
-            ))}
-        </Div>
+        <Container>
+            <StyledBoard>
+                {[...Array(boardSize * boardSize)].map((_, index) => (
+                    <Square
+                        key={index}
+                        x={index % boardSize}
+                        y={boardSize - Math.floor(index / boardSize) - 1}
+                    />
+                ))}
+            </StyledBoard>
+        </Container>
     );
 }
 
